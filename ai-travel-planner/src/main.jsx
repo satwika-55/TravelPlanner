668c3e6c3ea4f88a2643b7063cac8d703d0ef02e
satwika <chitrasatwika@gmail.com>
2025-05-17 10:34:35 +0530

    latest changes

--- ai-travel-planner/src/main.jsx
+++ ai-travel-planner/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/create-trip',
+    element: <CreateTrip />,
+  },
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </StrictMode>,
 )
