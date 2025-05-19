a1bc6f41830e8fefb190951a409166653ebef25e
satwika <chitrasatwika@gmail.com>
2025-05-19 15:26:45 +0530

    commit

--- travel-planner/src/components/custom/Hero.jsx
+++ travel-planner/src/components/custom/Hero.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function Hero() {
   return (
     <>
-    <div className = 'flex flex-col items-center mx-56 gap-9'>
+    <div className = 'flex flex-col items-center mx-56 gap-9 '>
         <h1 className = 'font-extrabold text-[60px] text-center mt-20' >
             <span className = 'text-[#f56551]'>Discover the power of travel planning</span> : with our AI-driven MakeYourTrip
         </h1>
@@ -14,8 +14,9 @@ function Hero() {
             <span className = 'font-bold '>Unleash Your Travel Beast!</span>
         </p>
         <Link to = '/create-trip'>
-        <Button className="font-bold text-[35px] border border-black bg-red-200 w-65 h-25 border-2 rounded-md">Let's Pack!</Button>
+        <Button className="font-bold text-[35px] border border-black bg-red-400 w-65 h-25 border-2 rounded-md">Let's Pack!</Button>
         </Link>
+        <img src = "https://wallpapers.com/images/featured/travel-ibk7fgrvtvhs7qzg.jpg"></img>
     </div>
     </>
   )
