d0813fb7c7c7cede3d324f84a02226f63289d9d8
satwika <chitrasatwika@gmail.com>
2025-06-02 17:41:13 +0530

    favicon and app title

--- travel-planner/src/App.jsx
+++ travel-planner/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,6 +8,19 @@ import Hero from './components/custom/Hero.jsx'
 
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Travel Planner";  // Set your desired title here
+  }, []);
+
+  useEffect(() => {
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = "image/svg+xml";
+  link.rel = "icon";
+  link.href = "/logo.svg"; // or /favicon.png
+  document.getElementsByTagName('head')[0].appendChild(link);
+  }, []);
+
 
   return (
     <>
