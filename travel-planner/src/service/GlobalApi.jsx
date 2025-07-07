0def65eea48c309068d9f4006733a1aa75ee6268
satwika <chitrasatwika@gmail.com>
2025-07-07 22:26:59 +0530

    places api

--- travel-planner/src/service/GlobalApi.jsx
+++ travel-planner/src/service/GlobalApi.jsx
@@ -1,8 +1,17 @@
+import axios from 'axios';
+
 const BASE_URL='https://places.googleapis.com/vi.places:searchText'
 
 const config = {
     headers:{
         'Content-Type':'application/json',
-        'X-Goog-Api-key': import.meta.env
+        'X-Goog-Api-key': import.meta.env.VITE_GEOAPIFY_API_KEY
+        'X-Goog-FieldMask':[
+            'places.photos',
+            'places.displayName',
+            'places.id'
+        ] 
     }
 }
+
+export const GetPlaceDetails=(data)=>axios.post(BASE_URL, data, config)
