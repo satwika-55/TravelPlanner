3bbe630450e724fabfbb4e507e7996b676155965
satwika <chitrasatwika@gmail.com>
2025-07-09 18:21:59 +0530

    geoapidy api added

--- travel-planner/src/service/GlobalApi.jsx
+++ travel-planner/src/service/GlobalApi.jsx
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
-const BASE_URL='https://places.googleapis.com/vi.places:searchText'
+const BASE_URL = 'https://api.geoapify.com/v1/geocode/autocomplete';
 
-const config = {
-    headers:{
-        'Content-Type':'application/json',
-        'X-Goog-Api-key': import.meta.env.VITE_GEOAPIFY_API_KEY
-        'X-Goog-FieldMask':[
-            'places.photos',
-            'places.displayName',
-            'places.id'
-        ] 
-    }
-}
-
-export const GetPlaceDetails=(data)=>axios.post(BASE_URL, data, config)
+export const GetPlaceDetails = async (textQuery) => {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        text: textQuery,
+        apiKey: import.meta.env.VITE_GEOAPIFY_API_KEY
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching place details from Geoapify:", error);
+    throw error;
+  }
+};
+export const PHOTO_REF_URL = 'https://photos.geoapify.com/v1/photo/{NAME}';
